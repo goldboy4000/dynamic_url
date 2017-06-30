876d4030635ba7a5c6c1dc7176d74311ad5d53c3
LaBestiaBY <helghastinquisitor@gmail.com>
2017-06-30 17:59:01 +0300

    added image, interface

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,14 @@
 /**
  * Created by alexander.andreev on 29-Jun-17.
  */
+
+(function (window)
+{
+    var frame = document.getElementsByClassName('frame');
+    var fork = document.getElementsByClassName('frame');
+    var fork = document.getElementsByClassName('frame');
+    var fork = document.getElementsByClassName('frame');
+    var fork = document.getElementsByClassName('frame');
+
+
+})(window);
